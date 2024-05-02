232fe6153fd72c93d2fc262a73ca1076bfb6b0ae
t34400 <34400mt@gmail.com>
2024-05-03 08:52:54 +0900

    Import scrcpy files from genymobile

---
+++ QuestScreenCapture/src/main/java/com/genymobile/scrcpy/DisplayInfo.java
@@ -0,0 +1,56 @@
+package com.genymobile.scrcpy;
+
+
+import android.util.Size;
+import androidx.annotation.NonNull;
+
+import java.util.Locale;
+
+public final class DisplayInfo {
+    // Added by @t-34400
+    private static final String format = "Id=%d, Size=(%d, %d), Rotation=%d, LayerStack=%d, Flags=%d";
+
+    private final int displayId;
+    private final Size size;
+    private final int rotation;
+    private final int layerStack;
+    private final int flags;
+
+    public static final int FLAG_SUPPORTS_PROTECTED_BUFFERS = 0x00000001;
+
+    public DisplayInfo(int displayId, Size size, int rotation, int layerStack, int flags) {
+        this.displayId = displayId;
+        this.size = size;
+        this.rotation = rotation;
+        this.layerStack = layerStack;
+        this.flags = flags;
+    }
+
+    public int getDisplayId() {
+        return displayId;
+    }
+
+    public Size getSize() {
+        return size;
+    }
+
+    public int getRotation() {
+        return rotation;
+    }
+
+    public int getLayerStack() {
+        return layerStack;
+    }
+
+    public int getFlags() {
+        return flags;
+    }
+
+    // Added by @t-34400
+    @NonNull
+    @Override
+    public String toString() {
+        return String.format(Locale.US, format, displayId, size.getWidth(), size.getHeight(), rotation, layerStack, flags);
+    }
+}
+
