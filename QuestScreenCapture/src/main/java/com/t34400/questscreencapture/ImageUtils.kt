8671618fe52341bae38ad757e5c8369315b1a8fe
t-34400 <34400mt@gmail.com>
2024-05-02 18:46:21 +0900

    Implement ImageUtils

---
+++ QuestScreenCapture/src/main/java/com/t34400/questscreencapture/ImageUtils.kt
@@ -0,0 +1,39 @@
+package com.t34400.questscreencapture
+
+import android.graphics.Bitmap
+import android.graphics.Color
+import android.media.Image
+import java.nio.ByteBuffer
+
+object ImageUtils {
+    fun convertRGBA8888toIntArray(image: Image): IntArray {
+        val planes = image.planes
+        val buffer = planes[0].buffer
+        val pixelStride = planes[0].pixelStride
+        val rowStride = planes[0].rowStride
+        val rowPadding = rowStride - pixelStride * image.width
+        val pixels = IntArray(image.width * image.height)
+        var offset = 0
+        for (row in 0 until image.height) {
+            for (col in 0 until image.width) {
+                val r = buffer[offset].toInt() and 0xff
+                val g = buffer[offset + 1].toInt() and 0xff
+                val b = buffer[offset + 2].toInt() and 0xff
+                val a = buffer[offset + 3].toInt() and 0xff
+                val pixel = Color.argb(a, r, g, b)
+                pixels[row * image.width + col] = pixel
+                offset += pixelStride
+            }
+            offset += rowPadding
+        }
+        return pixels
+    }
+
+    fun convertBitmapToIntArray(bitmap: Bitmap): IntArray {
+        val width = bitmap.width
+        val height = bitmap.height
+        val pixels = IntArray(width * height)
+        bitmap.getPixels(pixels, 0, width, 0, 0, width, height)
+        return pixels
+    }
+}
